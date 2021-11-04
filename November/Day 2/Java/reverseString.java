3b09eee778ce404afff699e527c6263cbd75d7f4
Trishal <mtrishal379@gmail.com>
2021-11-05 01:07:20 +0530

    Solution for StepsOnSteps in Java

--- November/Day 2/Java/reverseString.java
+++ November/Day 2/Java/reverseString.java
@@ -4,15 +4,20 @@ public class ReverseString {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a String ");
-        String n=sc.next();// we are taking a user input here
-        String rev=reverse(n);// We are passing the input to function which will reverse any string
+        //taking a user input
+        String n=sc.next();
+        // We are passing the input to function which will reverse any string
+        String rev=reverse(n);
         System.out.println(rev);
         sc.close();
     }
     public static String reverse(String str) {
-        String w="";// declaring and initialising a temp variable with blank,to store the value and return the string
+        // declaring and initialising a temp variable with blank,to store the value and return the string
+        String w="";
         for(int i=0;i<str.length();i++) {
-            w=str.charAt(i)+w; // Since it's a String,we can add any character at any part of the String...Here I am adding the character extracted,at the front of w to get the final reverse value 
+            // Since it's a String,we can add any character at any part of the String
+            //adding the character extracted,at the front of variable w to get the final reverse value
+            w=str.charAt(i)+w;
         }
         return w;
     }
--- November/Day 4/Java/StepsOnSteps.java
+++ November/Day 4/Java/StepsOnSteps.java
@@ -4,7 +4,8 @@ public class StepsOnSteps {
     public static void main(String args[]) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a Number");
-        int s=sc.nextInt(); // taking a User input as a number
+        // taking a User input as a number
+        int s=sc.nextInt();
         steps(s);
     }
     /*
@@ -14,10 +15,13 @@ public class StepsOnSteps {
         for(int i=1;i<=n;i++) {
             System.out.print("'");
             for(int j=1;j<=i;j++) {
-                System.out.print("#"); // The print command prints '#' one after another for every value of j < i..if i=5,then this will run 5 times and print ##### in a single line
+                // The print command prints '#' one after another for every value of j < i
+                // if i=5,then this will run 5 times and print ##### in a single line
+                System.out.print("#");
             }
             System.out.print("'");
-            System.out.println(); // to go to the next line
+            // to go to the next line
+            System.out.println();
         }
     }
 }
